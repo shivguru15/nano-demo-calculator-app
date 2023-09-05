b123b53bd5c31bcba40d23722981a10c8d0bb911
Shivguru_J <j.shivguru@gmail.com>
2023-09-06 00:14:47 +0530

    test1

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,53 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response{"Hello world!"}; // Return a greeting
 }
+
 crow::response add(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+    try {
+        auto input = crow::json::load(req.body);
+
+        if (!input) {
+            throw std::runtime_error("Invalid JSON input");
+        }
+
+        double first = input["first"].d();
+        double second = input["second"].d();
+
+        double result = first + second;
+
+        crow::json::wvalue response;
+        response["result"] = result;
+
+        return crow::response{crow::json::dump(response)};
+    } catch (const std::exception &e) {
+        return crow::response(400, e.what());
+    }
 }
+
 crow::response subtract(const crow::request &req)
 {
-    auto input = crow::json::load(req.body);
-    return crow::response{""};
+    try {
+        auto input = crow::json::load(req.body);
+
+        if (!input) {
+            throw std::runtime_error("Invalid JSON input");
+        }
+
+        double first = input["first"].d();
+        double second = input["second"].d();
+
+        double result = first - second;
+
+        crow::json::wvalue response;
+        response["result"] = result;
+
+        return crow::response{crow::json::dump(response)};
+    } catch (const std::exception &e) {
+        return crow::response(400, e.what());
+    }
 }
+
+
